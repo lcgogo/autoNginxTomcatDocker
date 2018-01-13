556a48616f0559093298f226c6001a84b2937ed9
lcgogo <lcgogo123@163.com>
2018-01-13 16:47:51 +0800

    ver0.1

--- atd.sh
+++ atd.sh
@@ -5,39 +5,46 @@
 WAR_FILE_NAME=yijava.war
 WAR_FOLDER=${WAR_FILE_NAME:0:-4}
 
+###############################################
+# function System_date
+function System_date(){
+echo `date +%Y-%m-%d-%H:%M:%S`
+}
+###############################################
+
 systemctl start docker
 if [ $? -ne 0 ];then
-  echo The docker is not installed. Install docker at first.
+  echo [`System_date`] The docker is not installed. Install docker at first.
   curl -fsSL https://get.docker.com/ | sh
   systemctl start docker
   else
-    echo The docker is installed.
+    echo [`System_date`] The docker is installed.
 fi
 
 tomcatExist=`docker image ls | grep tomcat`
 if [ -z "$tomcatExist" ];then
-  echo Tomcat docker image is not found. Pull it now.
+  echo [`System_date`] Tomcat docker image is not found. Pull it now.
   docker pull tomcat
   else
-    echo Tomcat docker image is existed.
+    echo [`System_date`] Tomcat docker image is existed.
 fi
 
 tomcatRunningID=`docker ps | grep catalina.sh | awk '{print $1}'`
 if [ -z "$tomcatRunningID" ];then
-  echo Tomcat docker is not running. Start it now.
+  echo [`System_date`] Tomcat docker is not running. Start it now.
   tomcatRunningID=`docker run -d -p 8080:8080 tomcat`
   else
-    echo Tomcat docker is running now.
-    echo The CONTAINER ID is $tomcatRunningID.
+    echo [`System_date`] Tomcat docker is running now.
+    echo [`System_date`] The CONTAINER ID is $tomcatRunningID.
 fi
 
 
-echo Copy $WAR_FILE_NAME to Tomcat.
+echo [`System_date`] Copy $WAR_FILE_NAME to Tomcat.
 docker cp $WAR_FILE_NAME $tomcatRunningID:/usr/local/tomcat/webapps
-echo Restart Tomcat.
+echo [`System_date`] Restart Tomcat.
 docker exec $tomcatRunningID /usr/local/tomcat/bin/catalina.sh start
 
-echo Sleep 10 seconds.
+echo [`System_date`] Sleep 10 seconds.
 sleep 10
 
 curl http://localhost:8080/$WAR_FOLDER/
