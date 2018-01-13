105d83fb4ac7de3e9b021549b9bd54c748344caf
lcgogo <lcgogo123@163.com>
2018-01-13 15:07:13 +0800

    ver0.1

--- atd.sh
+++ atd.sh
@@ -5,17 +5,39 @@
 WAR_FILE_NAME=yijava.war
 WAR_FOLDER=${WAR_FILE_NAME:0:-4}
 
-curl -fsSL https://get.docker.com/ | sh
-
 systemctl start docker
+if [ $? -ne 0 ];then
+  echo The docker is not installed. Install docker at first.
+  curl -fsSL https://get.docker.com/ | sh
+  systemctl start docker
+  else
+    echo The docker is installed.
+fi
 
-docker pull tomcat
+tomcatExist=`docker image ls | grep tomcat`
+if [ -z "$tomcatExist" ];then
+  echo Tomcat docker image is not found. Pull it now.
+  docker pull tomcat
+  else
+    echo Tomcat docker image is existed.
+fi
 
-CONTAINER_ID=`docker run -d -p 8080:8080 tomcat`
+tomcatRunningID=`docker ps | grep catalina.sh | awk '{print $1}'`
+if [ -z "$tomcatRunningID" ];then
+  echo Tomcat docker is not running. Start it now.
+  tomcatRunningID=`docker run -d -p 8080:8080 tomcat`
+  else
+    echo Tomcat docker is running now.
+    echo The CONTAINER ID is $tomcatRunningID.
+    echo Copy $WAR_FILE_NAME to Tomcat.
+    docker cp $WAR_FILE_NAME $tomcatRunningID:/usr/local/tomcat/webapps
+    echo Restart Tomcat.
+    docker exec $tomcatRunningID /usr/local/tomcat/bin/catalina.sh start
+fi
 
-docker cp $WAR_FILE_NAME $CONTAINER_ID:/usr/local/tomcat/webapps
-docker exec $CONTAINER_ID /usr/local/tomcat/bin/catalina.sh start
+
+echo Sleep 10 seconds.
+sleep 10
 
 curl http://localhost:8080/$WAR_FOLDER/
-
 
