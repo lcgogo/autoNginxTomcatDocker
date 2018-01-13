166c8cd484da3f3176b9cdd4cb9bcd92f86a98ad
lcgogo <lcgogo123@163.com>
2018-01-13 18:33:06 +0800

    ver0.2  Add file download.ver0.2  Add file download.

--- atd.sh
+++ atd.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # 2018.Jan.13th  Lewis Li  ver0.1  New script to deploy Tomcat in a Docker with local war file.
+# 2018.Jan.13th  Lewis Li  ver0.2  Add file download.
 
 WAR_FILE_NAME=yijava.war
+WAR_URL="https://github.com/lcgogo/autoTomcatDocker/raw/master/"
 WAR_FOLDER=${WAR_FILE_NAME:0:-4}
 
 ###############################################
@@ -11,6 +13,33 @@ function System_date(){
 echo `date +%Y-%m-%d-%H:%M:%S`
 }
 ###############################################
+
+###############################################
+# fucntion File_Download
+function File_Download(){
+  fileURL=$1
+  fileName=$2
+  fileNameTime=$fileName.`date +%Y%m%d%H%M%S`
+  fileNameNew=$fileNameTime.new
+  fileNameBak=$fileNameTime.bak
+  
+  wget -O $fileNameNew $fileURL$fileName
+
+  fileCRC=`cksum $fileName | awk '{print $1}'`
+  fileNewCRC=`cksum $fileNameNew | awk '{print $1}'`
+  if [ "$fileCRC" == "$fileNewCRC" ];then
+    echo [`System_date`] The downloaded $fileName is same as local. Exit now.
+    exit 0
+    else
+      echo [`System_date`] Backup the old file and rename the new one as now.
+      mv $fileName $fileNameBak
+      sleep 2
+      mv $fileNameNew $fileName
+  fi
+}
+###############################################
+
+File_Download $WAR_URL $WAR_FILE_NAME
 
 systemctl start docker
 if [ $? -ne 0 ];then
