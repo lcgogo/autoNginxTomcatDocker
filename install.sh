a451b6b6c416a49fa07a3816182bfacd3e962cd0
lcgogo <lcgogo123@163.com>
2018-01-20 13:11:31 +0800

    Optimize constant input

--- install.sh
+++ install.sh
@@ -32,76 +32,107 @@ if [ $? -ne 0 ];then
   exit 1
 fi
 
+chmod 755 $runFile
+mkdir -p $runFolder $runFolder/log
+\cp $runFile $runFolder
+
 ######################################
-# function Constant_Input
+# function Input_to_Constant
 #
+# Description
+# Used to get 4 CONSTANT: WAR_FILE_NAME WAR_URL ZIP_FILE_NAME ZIP_URL 
+# from screen input
+#
+# Input
+# $1 = fileType #should be war or zip
+# 
 # Return code
 # 0 success
 # 1 need reinput
-# 2 need confirm
-function Constant_Input(){
-  if [ ! -e $configFullPath ];then
-    echo Please input the location of war file:
-    echo \(For example: http://example.com/demo.war or /var/local/atnd/demo.war\)
-    read warUrl
-    WAR_FILE_NAME=`echo $warUrl | awk -F "/" '{print $NF}'`
-    WAR_URL=`echo ${warUrl:0:-${#WAR_FILE_NAME}}`
+# 2 error
+function Input_to_Constant(){
+  local fileType=$1
+  local inputUrl=""
+  local fileName=""
+  local fileUrl=""
+  local fileSuffix=""
+
+  echo -e Please input the location of "\033[32;49;1m $fileType \033[39;49;0m"  file:
+  echo \(For example: http://example.com/demo.$fileType or /var/local/atnd/demo.$fileType\)
+  read inputUrl
+  fileName=`echo $inputUrl | awk -F "/" '{print $NF}'`
+  fileUrl=`echo ${inputUrl:0:-${#fileName}}`
+  fileSuffix=`echo ${fileName:(-3)}`
     
-    echo Please input the location of zip file:
-    echo \(For example: http://example.com/demo.zip or /var/local/atnd/demo.zip\)
-    read zipUrl
-    ZIP_FILE_NAME=`echo $zipUrl | awk -F "/" '{print $NF}'`
-    ZIP_URL=`echo ${zipUrl:0:-${#ZIP_FILE_NAME}}`
-    
-    touch $configFullPath
-    echo WAR_FILE_NAME=$WAR_FILE_NAME > $configFullPath
-    echo WAR_URL=\"$WAR_URL\" >> $configFullPath
-    echo ZIP_FILE_NAME=$ZIP_FILE_NAME >> $configFullPath
-    echo ZIP_URL=\"$ZIP_URL\" >> $configFullPath
+  if [ $fileSuffix != $fileType ];then
+    echo Error input with wrong file type which should be end with $fileType.
+    echo -e Your input is : "\033[31;49;1m $inputUrl \033[39;49;0m"
+    return 1
   fi
     
-  echo You input is below, please confirm again
-  echo "#############"
-  cat $configFullPath
-  echo "#############"
-  echo -en "Please input Y/N: "
-  read choice
-  case $choice in
-    Y|y) echo Accept and continue.  
-         echo Your config is saved at $configFullPath.
-         echo You can run this script again to re-config.
-         return 0
-    ;;
-    N|n) echo Please input the file location again.
-         return 1
-    ;;
-    *) echo Invalid input. Please input Y or N. You input is $choice. 
-       echo Exit now without any changes. You can run this script again if needed.
-       return 2
-    ;;
-  esac
+  if [ $fileType = war ];then 
+    WAR_FILE_NAME=\"$fileName\"
+    WAR_URL=\"$fileUrl\"
+    return 0
+  elif [ $fileType = zip ];then
+    ZIP_FILE_NAME=\"$fileName\"
+    ZIP_URL=\"$fileUrl\"
+    return 0
+  else
+    echo Error input of function Constant_Input should be war or zip.
+    return 2
+  fi
 }
 ###############################
 
 ##################
 # CONSTANT INPUT #
 ##################
-Constant_Input
-constantInputResult=$?
-
-while [[ $constantInputResult -eq 1 || $constantInputResult -eq 2 ]]
+Input_to_Constant war
+warInputResult=$?
+while [[ $warInputResult -eq 1 || $warInputResult -eq 2 ]]
 do
-  Constant_Input
-  constantInputResult=$?
-  if [ $constantInputResult -eq 1 ];then
-    rm -f $configFullPath
-  fi
-  #echo constantInputResult is $constantInputResult
+  Input_to_Constant war
+  warInputResult=$?
 done
 
-chmod 755 $runFile
-mkdir -p $runFolder $runFolder/log
-\cp $runFile $runFolder
+Input_to_Constant zip
+zipInputResult=$?
+while [[ $zipInputResult -eq 1 || $zipInputResult -eq 2 ]]
+do
+  Input_to_Constant zip
+  zipInputResult=$?
+done
+
+if [ ! -e $configFullPath ];then
+  touch $configFullPath
+  echo WAR_FILE_NAME=$WAR_FILE_NAME > $configFullPath
+  echo WAR_URL=$WAR_URL >> $configFullPath
+  echo ZIP_FILE_NAME=$ZIP_FILE_NAME >> $configFullPath
+  echo ZIP_URL=$ZIP_URL >> $configFullPath
+fi
+
+#  echo You input is below, please confirm again
+#  echo "#############"
+#  cat $configFullPath
+#  echo "#############"
+#  echo -en "Is this input right? Input Y(y)/N(n): "
+#  read choice
+#  case $choice in
+#    Y|y|yes|Yes) echo Accept and continue.
+#         echo Your config is saved at $configFullPath.
+#         echo You can run this script again to re-config.
+#         return 0
+#    ;;
+#    N|n|no|No) echo Please input the file location again.
+#         rm -f $configFullPath
+#         return 1
+#    ;;
+#    *) echo Invalid input. Please input Y or N. Your input is $choice.
+#      # echo Exit now without any changes. You can run this script again if needed.
+#       return 2
+#    ;;
+#  esac
 
 cat > /usr/lib/systemd/system/$serviceFullName <<EOF
 [Unit]
